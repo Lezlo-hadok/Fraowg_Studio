2bbd592f3c3a5c11a41ca13b177d1716e9ddc600
Lezlo-hadok <lezlo6102@gmail.com>
2025-06-10 09:45:56 +0800

    commenting for scene change

--- Assets/scripts/menu/ChangeSceneScripts.cs
+++ Assets/scripts/menu/ChangeSceneScripts.cs
@@ -17,17 +17,17 @@ public class ChangeSceneScripts : MonoBehaviour
         SceneManager.LoadScene(sceneIndex);//change the scene by the chosen index
     }
 
-    public void ReloadCurrentScene()
+    public void ReloadCurrentScene() //to reload current scene
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //reload current when activiated scene
         
     }
 
-    public void LoadNextScene()
+    public void LoadNextScene() //loads the next scene in scen indext plus one from current
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //reloads current scene
     }
-    public void LoadPreviousScene()
+    public void LoadPreviousScene() //loads previous scene in scven index minus one from current scene
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);//loads the previous scene
     }
