bd06d6159bd2159207f449f2dcb3845b7b1935d6
Xman-C <xavier.cahir@gmail.com>
2025-06-11 15:48:22 +0800

    Modification Mouse look
    Mouse look can now read the inputs on the mouse and can now rotate and look up and down, sensitivity has been added to used to multiply the speed of which the camera moves

--- Assets/scripts/Movement/Mouse Look.cs
+++ Assets/scripts/Movement/Mouse Look.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
+    [Header("Sensitivity Settings")]
+    // Controls how fast the camera rotates
+    public float sensitivity = 8;
+
     [Header("Rotation Clamping")]
     // Limits vertical rotation 
     [SerializeField] Vector2 _verticalRotationClamp = new Vector2(-60, 60);
@@ -17,13 +21,16 @@ public class MouseLook : MonoBehaviour
     // Final vertical rotation value
     [SerializeField] float _verticalRotation;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
-        
+        // READ mouse x input ROTATE player on Y-axis using (Mouse X * sensitivity) 
+        _player.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        // READ mouse y inpute ADD (Mouse Y * sensitivity) to temporary rotation value 
+        _tempRotation += Input.GetAxis("Mouse Y") * sensitivity;
+        // CLAMP temporary rotation between min and max rotation limits 
+        _tempRotation = Mathf.Clamp(_tempRotation, _verticalRotationClamp.x, _verticalRotationClamp.y);
+
+        // APPLY vertical rotation to the camera's local X - axis
+        _camera.localEulerAngles = new Vector3(_verticalRotation, 0, 0);
     }
 }
