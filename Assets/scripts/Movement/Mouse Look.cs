8ef28c1ff6e8b068004253ec48c8b65c503dc222
Xman-C <xavier.cahir@gmail.com>
2025-06-11 15:44:56 +0800

    Modification Mouse look
    Mouse looks variables have been written into the code

--- Assets/scripts/Movement/Mouse Look.cs
+++ Assets/scripts/Movement/Mouse Look.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Rotation Clamping")]
+    // Limits vertical rotation 
+    [SerializeField] Vector2 _verticalRotationClamp = new Vector2(-60, 60);
+
+    [Header("Camera Setup")]
+    // Referance to the player object (for horizontal rotation)
+    [SerializeField] Transform _player;
+    // reference to the camera object (for vertical rotation)
+    [SerializeField] Transform _camera;
+    // Stores the temporary vertical rotation
+    [SerializeField] float _tempRotation;
+    // Final vertical rotation value
+    [SerializeField] float _verticalRotation;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
