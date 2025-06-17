d57494eef2a24ae101c565cd29e81d0c0094ac05
Lezlo-hadok <lezlo6102@gmail.com>
2025-06-17 16:54:47 +0800

    isdead correction and reapplying the project settings

--- Assets/scripts/Respawn/IsCharaterDead.cs
+++ Assets/scripts/Respawn/IsCharaterDead.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class IsCharaterDead : MonoBehaviour
 {
+    //is the charater alive 
     public  static bool isAlive;
+    //name of script
     internal static object isCharaterDead;
+    //character controler
     public CharacterController _characterController;
+    //on start set alive to true and check if charater is dead
     private void Start()
     {
         isAlive = true;
         CharacterDied();
     }
-
+    //is character in the death zone
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "DeathBubble")
@@ -18,6 +22,7 @@ public class IsCharaterDead : MonoBehaviour
             isAlive=false;    
         }
     }
+    //turn off/on the charater controler
     void CharacterDied()
     {
         CharacterController controller = _characterController;
