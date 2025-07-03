1773c356aaa5f02474b13b0dbe7c55584fe8f245
Xman-C <xavier.cahir@gmail.com>
2025-07-03 14:20:09 +0800

    TEST
    Test file

---
+++ Assets/Scripts/NEW.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NEW : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
